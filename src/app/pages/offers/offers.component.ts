5a9c429aef671846140a77c42a89b3d7b44d0360
VukasinVujic <vukasinvujic021@gmail.com>
2019-12-24 11:35:23 +0100

    adding unsubscribe

--- src/app/pages/offers/offers.component.ts
+++ src/app/pages/offers/offers.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {OfferService} from '../../offer.service';
-import { Observable, BehaviorSubject } from 'rxjs';
 import {Offer, ObjectOffer} from '../../Offer';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
   styleUrls: ['./offers.component.css']
 })
-export class OffersComponent implements OnInit {
+export class OffersComponent implements OnInit, OnDestroy {
   
+
   constructor(private offerService: OfferService) { }
 
-  offersList: ObjectOffer[] ;
-
+  offersList:  ObjectOffer[] ;
+  subscription: Subscription;
  
   ngOnInit() {
 
-    this.offerService.getOffers( ).subscribe(data => {
+    this.subscription = this.offerService.getOffers().subscribe(data => {
       this.offersList = [...data.offers];
     })
 
+  }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+
+    }
+
+  
   
   }
